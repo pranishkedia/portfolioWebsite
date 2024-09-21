87079266983b3abbaf5c0d2fee831fd5d5356973
pranishkedia <91530450+pranishkedia@users.noreply.github.com>
2024-09-21 20:43:40 +0100

    Template Setup

---
+++ my-website/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h2>Welcome to my portfolio</h2>
+      <p>This is a simple website created using React to display my projects and skills.</p>
+      
+      <h1>Coming Soon.</h1>
+
+    </section>
+  );
+};
+
+export default Home;
